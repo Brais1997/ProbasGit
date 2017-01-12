43a6df5aee1f0f938400f41e436c7ab7e05b1160
Brais Alvares Escudero <Brais Alvares Escudero>
2017-01-12 13:59:39 +0100

    proba

---
+++ main/java/com/example/balva/probasgit/Probas.java
@@ -0,0 +1,13 @@
+package com.example.balva.probasgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Probas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_probas);
+    }
+}
